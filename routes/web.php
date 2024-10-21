8095f3dc11ef2baffc974e629069e99b36ffd510
Anamika <anamikaswisstech@gmail.com>
2024-10-21 17:25:34 +0530

    service provider

--- routes/web.php
+++ routes/web.php
@@ -6,33 +6,32 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController; 
 
 
-    Route::get('/', function () {
-        return view('index');
-    })->middleware(['auth', 'verified'])->name('dashboard');
+    // Route::get('/', function () {
+    //     return view('index');
+    // })->middleware(['auth', 'verified'])->name('dashboard');
 
     Route::middleware('auth')->group(function () {
 
-    //for login and register    
-    Route::post('/index', function () {return view('index'); });  
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/profile/logout',[ProfileController::class, 'logout']);
+        //for dashboard
+        Route::get('/', function () {  return view('index');})->name('dashboard');   
 
+        // for login and register    
+        Route::post('/index', function () {return view('index'); });  
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::get('/profile/logout',[ProfileController::class, 'logout']);
 
-    //for sidebar
-    // for siderbar and header using service provider 
-    // Route::get('/', [MenuController::class, 'index'])->name('index');
-    
+        // for profile page
+        Route::get('/profile/user', [MenuController::class, 'profile'])->name('profile');
+        Route::post('/profile/save',[UserSettingController::class,'usersetting_save'])->name('user.save');
+        Route::put('/password/update', [UserSettingController::class, 'updatePassword'])->name('password.update');
+        
 
-    //for profile page
-    Route::get('/profile/user', [MenuController::class, 'profile'])->name('profile');
-    Route::post('/profile/save',[UserSettingController::class,'usersetting_save'])->name('user.save');
-    Route::put('/password/update', [UserSettingController::class, 'updatePassword'])->name('password.update');
-    
-
-
-    //Route::get('/', [MenuController::class, 'index'])->name('index'); 
-});
+        // for sidebar
+        // for siderbar and header using service provider 
+        // Route::get('/', [MenuController::class, 'index'])->name('index');
+            
+    });
 
 require __DIR__.'/auth.php';
